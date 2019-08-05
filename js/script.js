72a2a48da482e67d6b25efc238d70fd21775b245
Michael Hinte <michaelhinte@Michaels-MacBook-Pro.local>
2019-08-05 16:05:11 -0500

    Game logic complete, excluding special cases

--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
 /*----- constants -----*/ 
-
+const PLAYER = {
+    '1': 'One',
+    '-1': 'Two'
+};
 
 
 /*----- app's state (variables) -----*/ 
@@ -12,14 +15,15 @@ let board, scores, turn, winner;
 
 /*----- event listeners -----*/ 
 document.querySelector('section.a').addEventListener('click', handleClick);
-document.querySelector('section.b').addEventListener('click', handleClick);
+document.querySelector('section.b').addEventListener('click', handleClick2);
+// document.querySelector('section.b').addEventListener('click', handleClickTwo);
 
 
 /*----- functions -----*/
 init();
 
 function init() {
-    board = new Array(12).fill(4);
+    drawBoard();
     scores = {
         a: 0,
         b: 0
@@ -32,34 +36,72 @@ function render() {
     board.forEach(function(pot, potIdx) {
         document.getElementById(potIdx).innerHTML = pot;
     });
-
-    document.getElementById('a-score').innerHTML = scores.a;
-    document.getElementById('b-score').innerHTML = scores.b;
+    renderMessage();
 }
 
 function handleClick(evt) {
     let index = Number(evt.target.id);
     let value = board[index];
-
-    
-    for(let i = 0; i < value; i++) {
-        let newIndex = index + i + 1;
-        if (newIndex < board.length) {
-            board[newIndex] += 1;
-        } else {
-            newIndex -= 12;
-            board[newIndex] += 1;
-        }
-        
+    if (turn === -1 || index === 6 || value === 0) return;
+    else {
+        for(let i = 0; i < value; i++) {
+            let newIndex = index + i + 1;
+                if (newIndex < board.length - 1) {
+                    board[newIndex]++;
+                } else {
+                    newIndex -= board.length - 1;
+                    board[newIndex]++;
+                }
+            } 
+        board[evt.target.id] = 0;
+        turn *= -1;
+        render();
     }
-        
-    board[evt.target.id] = 0;
-    console.log(board);
-    render();
 }
 
-    
 
+function handleClick2(evt) {
+    let index = Number(evt.target.id);
+    let value = board[index];
+    if (turn === 1 || index === 13 || value === 0) return;
+    else {
+        for(let i = 0; i < value; i++) {
+            let newIndex = index + i + 1;
+            
+            if (newIndex < board.length) {
+                board[newIndex]++;
+                
+            } else if (newIndex >= board.length){
+                newIndex -= board.length;
+                board[newIndex]++;
+            }
+            
+            if (newIndex === 6) {
+                board[newIndex]--;
+                value++;
+            }
+                
+                
+               
+            } 
+        board[evt.target.id] = 0;
+        turn *= -1;
+        render();
+    }
+}
+
+
+function drawBoard(){
+    board = new Array(14).fill(4);
+    board[6] = 0;
+    board[13] = 0;
+}
+            
+            
+            
+function renderMessage(){
+    document.querySelector('footer').innerHTML = `Player ${PLAYER[turn]}'s Turn!`;
+}
     
     
 
